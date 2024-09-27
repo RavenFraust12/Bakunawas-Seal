07a5c89d45b9a417c4bd28b0d1ab928764add9fd
RavenFraust12 <tjvangeles08@gmail.com>
2024-09-27 15:44:26 +0800

    Temporary Commit

--- Bakunawa's Seal/Assets/Scripts/SpawnManager.cs
+++ Bakunawa's Seal/Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,83 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Spawn")]
+    public GameObject[] unitsToSpawn;
+    public GameObject[] spawnPoint;
+    public float spawnInterval;
+    public float minSpawnInterval;
+
+    [Header("Wave")]
+    public int waveCount;
+    public int currentUnitCount;
+    public int spawnedUnitThisWave;
+    public int maxUnitCount;
+    public int destroyedUnits;
+    public bool onGoingWave;
+
+
+    private void Start()
     {
-        
+        StartCoroutine(SpawnWaveRoutine());
     }
 
-    // Update is called once per frame
-    void Update()
+    void Spawn()
     {
-        
+        currentUnitCount++;
+        spawnedUnitThisWave++;
+        float spawnPoint_x = Random.Range(0, 100);
+        float spawnPoint_y = Random.Range(0, 100);
+        Vector3 whereToSpawn = new Vector3(spawnPoint_x, spawnPoint_y, 0.5f);
+        //int spawnPointIndex = Random.Range(0, spawnPoint.Length);
+        int randomEnemyIndex = Random.Range(0, unitsToSpawn.Length);
+
+        //Instantiate(unitsToSpawn[randomEnemyIndex], spawnPoint[spawnPointIndex].transform.position, Quaternion.identity);
+        Instantiate(unitsToSpawn[randomEnemyIndex], whereToSpawn, Quaternion.identity);
+
+        if (spawnedUnitThisWave >= maxUnitCount)
+        {
+            onGoingWave = false;
+        }
+    }
+
+    IEnumerator SpawnWaveRoutine()
+    {
+        while (true)
+        {
+            if (!onGoingWave && currentUnitCount == 0)
+            {
+                StartNewWave();
+            }
+
+            if (onGoingWave && spawnedUnitThisWave < maxUnitCount)
+            {
+                yield return new WaitForSeconds(spawnInterval);
+                Spawn();
+            }
+
+            yield return null;
+        }
+    }
+    void StartNewWave()
+    {
+        Debug.Log("Starting Wave: " + waveCount);
+        onGoingWave = true;
+        spawnedUnitThisWave = 0;
+
+        maxUnitCount += 10;
+        spawnInterval = Mathf.Max(minSpawnInterval, spawnInterval - 0.2f);
+    }
+
+    public void EnemyDestoryed()
+    {
+        currentUnitCount--;
+        destroyedUnits++;
+
+        if (currentUnitCount <= 0 && !onGoingWave && spawnedUnitThisWave >= maxUnitCount)
+        {
+            waveCount++;
+            StartNewWave();
+        }
+
     }
 }
